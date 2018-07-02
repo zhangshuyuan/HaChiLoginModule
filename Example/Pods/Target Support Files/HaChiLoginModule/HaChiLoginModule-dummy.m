6e0f020eb1a06b8e293c54542512b89823ba1162
zhangshuyuan <961048968@qq.com>
2018-07-02 17:42:48 +0800

    1.体检

---
+++ Example/Pods/Target Support Files/HaChiLoginModule/HaChiLoginModule-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HaChiLoginModule : NSObject
+@end
+@implementation PodsDummy_HaChiLoginModule
+@end
---
+++ Example/Pods/Target Support Files/Pods-HaChiLoginModule_Tests/Pods-HaChiLoginModule_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HaChiLoginModule_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_HaChiLoginModule_Tests
+@end
